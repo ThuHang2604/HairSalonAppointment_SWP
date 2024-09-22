a305b82205d0684827b693ea327a1c20880fce97
ThuHang2604 <132872569+ThuHang2604@users.noreply.github.com>
2024-09-22 17:45:36 +0700

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,30 @@
-import logo from './logo.svg';
+import { Routes, Route } from 'react-router-dom';
+import { publicRoutes } from './routes/routes';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Routes>
+        {/* Puclic routes */}
+        <Route>
+          {publicRoutes.map((item, index) => {
+            const Layout = item.layout || DefaultLayout;
+            const Page = item.component;
+            return (
+              <Route
+                key={index}
+                path={item.path}
+                element={
+                  <Layout>
+                    <Page />
+                  </Layout>
+                }
+              />
+            );
+          })}
+        </Route>
+      </Routes>
     </div>
   );
 }
---
+++ src/routes/routes.js
@@ -0,0 +1,13 @@
+import HomePage from '@/pages/HomePage';
+
+const publicRoutes = [
+  {
+    title: 'Home',
+    path: '/',
+    component: HomePage,
+  },
+];
+
+const adminRoutes = [];
+
+export { publicRoutes, adminRoutes };
